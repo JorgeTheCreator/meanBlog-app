5d5a02a3f07cdf34c3ddb2821e389df4e89bf9f9
Jorge Peguero <JrgPeguero@yahoo.com>
2017-02-01 13:28:56 -0500

    add 4 forms

--- modules/posts/server/models/post.server.model.js
+++ modules/posts/server/models/post.server.model.js
@@ -10,10 +10,27 @@ var mongoose = require('mongoose'),
  * Post Schema
  */
 var PostSchema = new Schema({
-  name: {
+  title: {
     type: String,
     default: '',
     required: 'Please fill Post name',
+    trim: true
+  },
+  body: {
+    type: String,
+    default: '',
+    required: 'Hey, Einstein, you forgot to fill the body',
+    trim: true
+  },
+  major: {
+    type: String,
+    default: '',
+    trim: true
+  },
+  category: {
+    type: String,
+    default: '',
+    required: 'Hey, Einstein, you forgot to pick a category',
     trim: true
   },
   created: {
